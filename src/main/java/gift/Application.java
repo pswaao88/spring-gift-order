61b8c15ac73ac38a0eb62f30e5d68207e03c7a97
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-07-23 18:24:59 +0900

    Feat: KakaoProperties 생성 및 테스트

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,8 +1,13 @@
 package gift;
 
+import gift.Model.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(KakaoProperties.class)
+@ConfigurationPropertiesScan
 @SpringBootApplication()
 public class Application   {
     public static void main(String[] args) {
--- src/test/java/gift/RestClientTest.java
+++ src/test/java/gift/RestClientTest.java
@@ -2,9 +2,11 @@ package gift;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import gift.Model.KakaoProperties;
 import java.net.URI;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,15 +15,18 @@ import org.springframework.web.client.RestClient;
 
 @SpringBootTest
 public class RestClientTest {
+    @Autowired
+    private KakaoProperties kakaoProperties;
+
     private final RestClient client = RestClient.builder().build();
 
     @Test
     void test1(){
         var url = "https://kauth.kakao.com/oauth/token";
-        var body = new LinkedMultiValueMap<String, String>();
+        var body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
-        body.add("client_id","");
-        body.add("redirect_url","http://localhost:8080");
+        body.add("client_id",kakaoProperties.getClientId());
+        body.add("redirect_url",kakaoProperties.getRedirectUrl());
         body.add("code","9Is5pfoVl6Jb-BvI95BTIyskRIq73yGEynvl6ObqNhpqaxUFQmHinAAAAAQKKiWOAAABkNg9QiVV7imzm104lw"); // 변수로 뺴야됨
         var response = client.post()
             .uri(URI.create(url))
@@ -32,5 +37,11 @@ public class RestClientTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         System.out.println(response);
+
+    }
+    @Test
+    void test2(){
+        System.out.println(kakaoProperties.getClientId());
+        System.out.println(kakaoProperties.getRedirectUrl());
     }
 }
